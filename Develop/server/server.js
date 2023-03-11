bee59cd4b928ca01c6b9a4459d2d99b99506b2ff
Ryan Carless <ryancarless96@gmail.com>
2023-03-11 14:48:53 -0500

    add server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+const {authMiddleware} = require('./utils/auth');
+const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +21,19 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  app.use(routes);
+  
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
 
 app.use(routes);
 
