e5feb70811ec6719e0de4154f7e30e91f33f6992
Ryan Carless <ryancarless96@gmail.com>
2023-03-22 15:06:25 -0400

    make it better

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState, useEffect } from 'react';
-// import { ApolloProvider } from '@apollo/client';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import {useMutation} from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -12,10 +11,10 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
-  const[login, {error}] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
-  useEffect(()=> {
-    if(error) {
+  useEffect(() => {
+    if (error) {
       setShowAlert(true);
     } else {
       setShowAlert(false);
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import {gql} from '@apollo/client';
+import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
@@ -27,6 +27,25 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
     mutation saveBook($bookData: BookInput!) {
         saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
             _id
             username
             email
